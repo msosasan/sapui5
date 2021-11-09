e68ca71e0658c371ab65ee808d099c540782ba5c
msosasan <marco.sosa@t-systems.com>
2021-11-09 00:20:13 +0000

    third commit on msosasan

--- Invoices/SAPUI5/webapp/controller/App.controller.js
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -1,22 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ],
     /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
-   * @param {typeof sap.m.MessageToast} MessageToast
    */
-    function (Controller, MessageToast) {
+    function (Controller) {
         "use strict";
         return Controller.extend("logaligroup.SAPUI5.controller.App", {
             onInit: function () {
                
             },
-            onShowHello: function () {
-                let oBundle = this.getView().getModel("i18n").getResourceBundle();
-                let sRecipient =  this.getView().getModel().getProperty("/recipient/name");
-                let sMessage = oBundle.getText("helloMessage", [sRecipient]);
-                MessageToast.show(sMessage);
+            onOpenDialogHeader: function() {
+                  this.getOwnerComponent().openHelloDialog();
             }
         })
 
